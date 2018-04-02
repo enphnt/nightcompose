dad215d04ccfb1215ad604f4a003bcf986745761
Nathan Phennel <nathanphennel@gmail.com>
2018-04-02 19:44:42 +1000

    feat: nightwatch diddle one

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  open: false,
+  logLevel: "silent",
+  server: {
+    middleware: {
+      0: null
+    }
+  }
+};
---
+++ nightwatch.conf.js
@@ -0,0 +1,52 @@
+const seleniumServer = require("selenium-server");
+const chromedriver = require("chromedriver");
+
+module.exports = {
+  "src_folders": ["compositions"],
+  "custom_commands_path": "commands",
+  "page_objects_path": "pages",
+  "selenium": {
+    "start_process": false,
+    "server_path": seleniumServer.path,
+    "host": "127.0.0.1",
+    "port": 4444,
+    "cli_args": {
+      "webdriver.chrome.driver": chromedriver.path
+    }
+  },
+  "test_workers": true,
+  "test_settings": {
+    "default": {
+      "launch_url": "",
+      "selenium_host": "localhost",
+      "selenium_port": 4444,
+      "pathname": "/wd/hub",
+      "desiredCapabilities": {
+        "browserName": "chrome"
+      },
+      "globals": {
+        beforeEach: function (client, done) {
+          client.status(function (result) {
+            client.url("localhost:3000");
+            done();
+          });
+          client.perform(() => {
+            client.url("localhost:3000")
+              .waitForElementVisible("body > canvas", 3000);
+            done();
+          });
+        },
+        afterEach: function (client, done) {
+          client.end(function () {
+            done();
+          });
+        },
+        one: 100,
+        two: 200,
+        three: 300,
+        four: 400,
+        eight: 800
+      }
+    }
+  }
+};
